227e9c6f0d0eec42871f3a33ffd077f38aa8090c
R0Bill <157518506+R0Bill@users.noreply.github.com>
2024-01-28 21:00:26 +0800

    2024.1.28 update add 1 card

---
+++ src/main/java/rosmod/cards/skill/Intimidate.java
@@ -0,0 +1,39 @@
+package rosmod.cards.skill;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import rosmod.cards.BaseCard;
+import rosmod.character.MyCharacter;
+import rosmod.util.CardStats;
+
+public class Intimidate extends BaseCard {
+    public static final String ID = makeID("Intimidate");
+    private static final CardStats info = new CardStats(
+            MyCharacter.Enums.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or something similar for a basegame character color.
+            CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.RARE, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.ALL_ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            2 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+    );
+
+    public Intimidate() {
+        super(ID, info);
+        setExhaust(true);
+        setCostUpgrade(1);
+    }
+
+    @Override
+    public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
+        for (AbstractMonster mo : (AbstractDungeon.getCurrRoom()).monsters.monsters) {
+            AbstractDungeon.actionManager.addToBottom((AbstractGameAction) new ApplyPowerAction((AbstractCreature) mo, (AbstractCreature) abstractPlayer, (AbstractPower) new VulnerablePower((AbstractCreature) mo, 99, false), 99, true));
+            AbstractDungeon.actionManager.addToBottom((AbstractGameAction) new ApplyPowerAction((AbstractCreature) mo, (AbstractCreature) abstractPlayer, (AbstractPower) new WeakPower((AbstractCreature) mo, 99, false), 99, true));
+        }
+    }
+}
